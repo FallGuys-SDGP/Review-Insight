0a3354d11b452f9e96535da75163791c64472c0b
Uwin-Asus-Laptop_2 <fb.uwin@gmail.com>
2021-03-02 11:04:01 +0530

    RI-2 #center search bar #Code-Review

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import "../../components/home/Home.css";
-import { TextField } from '@material-ui/core';
+import { TextField,Button } from '@material-ui/core';
 
 function Home(){
   return(
     <div className="home">
-      {/* <div class="wrapper">
-        <input class="search" placeholder="Search..." type="text" />
-        <button type="submit" >Submit</button>
-      </div>  */}
+      <div className="wrapper">
+        {/* <input class="search" placeholder="Search..." type="text" />
+        <button type="submit" >Submit</button> */}
+        <TextField className="Searchbar" id="outlined-basic" label="" variant="outlined" />
+        <Button id="outlined-basic" label="Outlined" variant="contained" > Search </Button>
+      </div> 
 
-     
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+
       
     </div>
   );
