bdf8e0d373363dc7c0a72eca2a6d27ff0d3f84c0
Uwin-Asus-Laptop_2 <fb.uwin@gmail.com>
2021-03-09 02:08:10 +0530

    RI-14 #comment add lambda functional code

--- backend/amplify/backend/function/getProduct/src/index.js
+++ backend/amplify/backend/function/getProduct/src/index.js
@@ -1,15 +1,38 @@
+'use strict'
+const AWS = require('aws-sdk')
 
+AWS.config.update({ region: "ap-south-1"})
 
 exports.handler = async (event) => {
-    // TODO implement
+    const db = new AWS.DynamoDB( {apiVersion: "2012-10-08"})
+    const documentClient = new AWS.DynamoDB.DocumentClient({ region: "ap-south-1"});
+    
+    let responseBody = "";
+    let statusCode = 0;
+
+    const {asin} = event.pathParameters;
+
+    const params = {
+        TableName: "Product",
+        Key: {
+            asin: asin
+        }
+    }
+    try {
+        const data = await documentClient.get(params).promise();
+        responseBody = JSON.stringify(data.Item);
+        statusCode = 200;
+    } catch (err) {
+        responseBody = "Unable to get product data";
+        statusCode = 403;
+    }
+    
     const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+        statusCode : statusCode,
+        headers: {
+            "myHeader": "test"
+        },
+        body: responseBody
+    }
     return response;
 };
