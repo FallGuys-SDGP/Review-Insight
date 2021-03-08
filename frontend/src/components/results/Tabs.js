7f7f4c776f473303acde9d90765a36146337c246
Viraj Shakya Samaranayake <virajofficialinfo@gmail.com>
2021-03-08 20:22:50 +0530

    Draft main features in specification

--- frontend/src/components/results/Tabs.js
+++ frontend/src/components/results/Tabs.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import "../results/Tabs.css";
 import ReviewContainer from "./ReviewContent/ReviewContainer";
 
+
 function Tabs() {
   const [toggleState, setToggleState] = useState(1);
 
@@ -49,7 +50,119 @@ function Tabs() {
         </div>
 
         <div className={toggleState === 3 ? "content  active-content" : "content"}>
-          <p>test 1</p>
+
+        <h1><a>Apple EarPods</a>Specifications</h1>
+
+        
+
+        <table>
+            <h2>General</h2>
+
+            <tr>
+              <th>Brand</th>
+              <td>Apple</td>
+            </tr>
+            <tr>
+              <th>Model</th>
+              <td>AirPods Max Wireless Headphones</td>
+            </tr>
+            <tr>
+              <th>Price</th>
+              <td>₹59,900</td>
+            </tr>
+            <tr>
+              <th>Launch Date</th>
+              <td>December 2020</td>
+            </tr>
+            <tr>
+              <th>Model Number</th>
+              <td>AirPods Max</td>
+            </tr>
+            <tr>
+              <th>Type</th>
+              <td>Headphones</td>
+            </tr>
+            <tr>
+              <th>Headphone Type</th>
+              <td>Over-Ear</td>
+            </tr>
+            <tr>
+              <th>Connectivity</th>
+              <td>Wireless</td>
+            </tr>
+
+
+
+            <h2>Product details</h2>
+
+            <tr>
+              <th>Microphone</th>
+              <td>Yes</td>
+            </tr>
+            <tr>
+              <th>Driver Type</th>
+              <td>Dynamic</td>
+            </tr>
+            <tr>
+              <th>Driver Size (mm)</th>
+              <td>40</td>
+            </tr>
+            <tr>
+              <th>Number of Drivers</th>
+              <td>1</td>
+            </tr>
+            <tr>
+              <th>Water Resistant</th>
+              <td>No</td>
+            </tr>
+            <tr>
+              <th>Weight (g)</th>
+              <td>385.00</td>
+            </tr>
+
+         
+
+
+
+
+            <h2>Connectivity features</h2>
+
+            <tr>
+                <th>Bluetooth</th>
+                <td>Yes</td>
+            </tr>
+
+            <tr>
+                <th>Bluetooth Version</th>
+                <td>5</td>
+            </tr>
+
+            <tr>
+                <th>Bluetooth Codec Support</th>
+                <td>SBC,AAC</td>
+            </tr>
+
+            <tr>
+                <th>Battery</th>
+                <td>features</td>
+            </tr>
+
+            <tr>
+                <th>Charging Port</th>
+                <td>Proprietary</td>
+            </tr>
+
+
+            <tr>
+                <th>Battery Life (Hrs)</th>
+                <td>20</td>
+            </tr>
+          </table>
+
+
+          
+          
+          
         </div>
       </div>
     </div>
