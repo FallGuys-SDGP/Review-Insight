b8947faa2aa67f706139cd14bbd2e78d3840657f
shanthosh-srikan <shanthosh.2016261@iit.ac.lk>
2021-03-09 01:49:19 +0530

    footer created

---
+++ frontend/src/components/footer/Footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Footer.css";
+import Typography from '@material-ui/core/Typography';
+import faceBook from '../asserts/footer/facebook.png';
+import instagram from '../asserts/footer/insta.jpeg';
+import twitter from '../asserts/footer/twitter.png';
+
+
+
+function Footer(){
+  return(
+   
+   <div  className = "wrapper-footer" >
+        <div className="container-footer">  
+            <div className = "item-footer"> 57 Ramakrishna Road,Colombo 6</div>
+            <div className = "item-footer"> +94 112 345567</div>
+            <div className = "item-footer"> review-insight@gmail.com</div>
+        </div>
+        
+        <div>
+              <div className="container-footer"> 
+                <div className = "item-footer"> About the Comapany</div>
+                <div className = "item-footer"> We are a Company try to help Online customer to buy genuie product.</div>
+              </div>
+              
+              <div>
+              
+                <div className= 'social-icon'><img src={faceBook} alt="userIcon" height="40px" /></div>
+                {/* <div className= 'social-icon'><img src={instagram} alt="userIcon" height="40px" /></div>
+                <div className= 'social-icon'><img src={twitter} alt="userIcon" height="40px" /></div> */}
+
+              </div>
+
+         </div>
+        
+     
+         
+    </div> 
+  );
+
+    
+}
+
+export default Footer;
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import "../../components/home/Home.css";
 import { TextField,Button } from '@material-ui/core';
 import SearchBar from '../../components/home/SearchBar';
+import Footer from '../footer/Footer'
 
 function Home(){
   return(
     <div className="home">
       <SearchBar />
+      <Footer/>
     </div>
   );
 }
