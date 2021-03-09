0a5ae9a5ffb24eac71504f6256760784200f42a7
Navod Zoysa <navod.zoysa@gmail.com>
2021-03-09 22:38:55 +0530

    RI-10 #comment added doughnut chart to summary component

--- frontend/src/components/results/Result.js
+++ frontend/src/components/results/Result.js
@@ -2,9 +2,30 @@ import React from "react";
 import Tabs from "./Tabs";
 import { Card } from '@material-ui/core';
 import "../results/Result.css";
+import {Doughnut} from "react-chartjs-2";
 
 const productName = "Apple iPhone 12";
 const productImage = "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-12-pro-r1.jpg"
+const summaryData = {
+	labels: [
+		'Positve',
+		'Negative',
+		'Fake'
+	],
+	datasets: [{
+		data: [300, 100, 50],
+		backgroundColor: [
+		'#36A2EB',
+		'#FFCE56',
+		'#FF6384'
+		],
+		hoverBackgroundColor: [
+		'#36A2EB',
+		'#FFCE56',
+		'#FF6384'
+		]
+	}]
+};
 
 function Result(){
   return(
@@ -16,7 +37,10 @@ function Result(){
             <img className="res-product-img" src={productImage}/>
           </div>
           <div className="summary">
-            <h1 className="summary-title"> Summary </h1>
+            <h1 className="summary-title">Summary</h1>
+            <div className="summary-chart-box">
+              <Doughnut data={summaryData}></Doughnut>
+            </div>
           </div>
         </div>
         <div>
