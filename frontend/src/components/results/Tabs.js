2c6c6c0fd1c8ce88b0f777b7a572937e917071f3
Viraj Shakya Samaranayake <virajofficialinfo@gmail.com>
2021-03-09 16:35:52 +0530

    RI-28 #initiated #comment Created user API

--- frontend/src/components/results/Tabs.js
+++ frontend/src/components/results/Tabs.js
@@ -49,7 +49,124 @@ function Tabs() {
         </div>
 
         <div className={toggleState === 3 ? "content  active-content" : "content"}>
+<<<<<<< Updated upstream
           <p>Specification Content Here</p>
+=======
+
+        <h1><a>Apple EarPods</a> Specifications</h1>
+
+        
+        <img src="apple.jpg" alt="product specification"></img>
+
+        <table>
+            <h2>General</h2>
+
+            <tr>
+              <th>Brand</th>
+              <td>Apple</td>
+            </tr>
+            <tr>
+              <th>Model</th>
+              <td>AirPods Max Wireless Headphones</td>
+            </tr>
+            <tr>
+              <th>Price</th>
+              <td>₹59,900</td>
+            </tr>
+            <tr>
+              <th>Launch Date</th>
+              <td>December 2020</td>
+            </tr>
+            <tr>
+              <th>Model Number</th>
+              <td>AirPods Max</td>
+            </tr>
+            <tr>
+              <th>Type</th>
+              <td>Headphones</td>
+            </tr>
+            <tr>
+              <th>Headphone Type</th>
+              <td>Over-Ear</td>
+            </tr>
+            <tr>
+              <th>Connectivity</th>
+              <td>Wireless</td>
+            </tr>
+
+
+
+            <h2>Product details</h2>
+
+            <tr>
+              <th>Microphone</th>
+              <td>Yes</td>
+            </tr>
+            <tr>
+              <th>Driver Type</th>
+              <td>Dynamic</td>
+            </tr>
+            <tr>
+              <th>Driver Size (mm)</th>
+              <td>40</td>
+            </tr>
+            <tr>
+              <th>Number of Drivers</th>
+              <td>1</td>
+            </tr>
+            <tr>
+              <th>Water Resistant</th>
+              <td>No</td>
+            </tr>
+            <tr>
+              <th>Weight (g)</th>
+              <td>385.00</td>
+            </tr>
+
+         
+
+
+
+
+            <h2>Connectivity features</h2>
+
+            <tr>
+                <th>Bluetooth</th>
+                <td>Yes</td>
+            </tr>
+
+            <tr>
+                <th>Bluetooth Version</th>
+                <td>5</td>
+            </tr>
+
+            <tr>
+                <th>Bluetooth Codec Support</th>
+                <td>SBC,AAC</td>
+            </tr>
+
+            <tr>
+                <th>Battery</th>
+                <td>features</td>
+            </tr>
+
+            <tr>
+                <th>Charging Port</th>
+                <td>Proprietary</td>
+            </tr>
+
+
+            <tr>
+                <th>Battery Life (Hrs)</th>
+                <td>20</td>
+            </tr>
+          </table>
+
+
+          
+          
+          
+>>>>>>> Stashed changes
         </div>
       </div>
     </div>
