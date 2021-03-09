6a9f3da452ec85ca29d61eecc1ff83dba63751a8
Navod Zoysa <navod.zoysa@gmail.com>
2021-03-09 21:38:35 +0530

    RI-10 added a product summary card above summary

--- frontend/src/components/results/Result.js
+++ frontend/src/components/results/Result.js
@@ -6,8 +6,13 @@ import "../results/Result.css";
 function Result(){
   return(
     <div className="result-container">
-        <div className="summary">
-          <h1 className="summary-title"> Summary </h1>
+        <div className="result-sidebar">
+          <div className="res-product-summary">
+            <h1 className="res-product-summary-title">Product</h1>
+          </div>
+          <div className="summary">
+            <h1 className="summary-title"> Summary </h1>
+          </div>
         </div>
         <div>
           <Tabs />
