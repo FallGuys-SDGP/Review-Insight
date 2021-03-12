f187f2e82adff3e3c92ffd254b2eff69e224fd43
Uwin-Asus-Laptop_2 <fb.uwin@gmail.com>
2021-03-13 02:43:29 +0530

    fix file names and image import issue

--- frontend/src/components/about-us/aboutus.js
+++ frontend/src/components/about-us/aboutus.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "../about-us/aboutus.css";
-import uwin from "../image/members/uwin.jpeg"
-//import{TextField,Button} from '@material-ui/core';
+import uwin from "../images/members/uwin.jpg"
+import navod from "../images/members/navod.jpg"
+import shanthosh from "../images/members/shanthosh.jpg"
 
 function AboutUs(){
     return(
         <div className = "about-us">
-
             <div>
                 <h1 class ="nameIntro"> Introduction</h1>
             </div>
-
+            
             <div>
                 <h1 class = "nameMission">Mission</h1>
             </div>
@@ -27,12 +27,12 @@ function AboutUs(){
                 </div>
 
                 <div class = "navod">
-                    <img src = "images/members/navod.jpg"   alt="Navod"/>
+                    <img src = {navod}  alt="Navod"/>
                     <p class = "fontType">Navod</p>
 				</div>
 
 				<div class = "shanthosh">
-                    <img src = "images/members/shanthosh.jpg"   alt="Shanthosh"/>
+                    <img src = {shanthosh}   alt="Shanthosh"/>
                     <p class = "fontType">Shanthosh</p>
                 </div>
 
