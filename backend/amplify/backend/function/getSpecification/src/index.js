a5d46e7729101e0c6276e6440a1495c629f913cf
Viraj Shakya Samaranayake <virajofficialinfo@gmail.com>
2021-04-16 14:04:30 +0530

    finalized  specification lambda function and API

--- backend/amplify/backend/function/getSpecification/src/index.js
+++ backend/amplify/backend/function/getSpecification/src/index.js
@@ -3,26 +3,43 @@
 'use strict'
 const AWS = require('aws-sdk');
 
-AWS.config.update({ region: "ap-south-1"});
+AWS.config.update({ region: "ap-south-1" });
 
 exports.handler = async (event, context) => {
-  const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08"});
-  const documentClient = new AWS.DynamoDB.DocumentClient({ region: "ap-south-1"});
-  
-  const params = {
-    TableName: "specification",
-    Key: {
-        Model: "ATH-AR5 Wired Headphones"
-    }
-  }
+    const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+    const documentClient = new AWS.DynamoDB.DocumentClient({ region: "ap-south-1" });
 
-  try {
-    const data = await documentClient.get(params).promise();
-    console.log(data);
-    return data;
-  } catch (err) {
-    console.log(err);
-    
-  }
+    let responseBody = "";
+    let statusCode = 0;
+
+    const { asin } = event.pathParameters;
+    const params = {
+        TableName: "specification",
+        Key: {
+            asin: asin
+        }
+    }
+
+    try {
+        const data = await documentClient.get(params).promise();
+        responseBody = JSON.stringify(data.Item);
+        statusCode = 200;
+    } catch (err) {
+        responseBody = "Unable to get product data";
+        statusCode = 403;
+    }
+
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "myHeader": "test",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+            "content-type": "text/html"
+        },
+        body: responseBody
+    }
+    return response;
 }
 
