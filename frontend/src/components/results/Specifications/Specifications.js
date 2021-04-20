6afe09d838efd8a4919977acd753b8edd71ce5f1
Viraj Shakya Samaranayake <virajofficialinfo@gmail.com>
2021-04-20 20:49:18 +0530

    work chart

--- frontend/src/components/results/Specifications/Specifications.js
+++ frontend/src/components/results/Specifications/Specifications.js
@@ -1,138 +1,97 @@
 import React from "react";
 import "./Specifications.css"
 import logo from '../../../../src/images/temp/appleImage.jpg';
-
-const Brand="Apple"
-const Model="AirPods Max Wireless Headphones"
-const Price="₹59,900"
-const Launch_Date="December 2020"
-const Model_Number="AirPods Max"
-const Type="Headphones"
-const Headphone_Type="Over-Ear"
-const Connectivity="Wireless"
-const Microphone="Yes"
-const Driver_Type="Dynamic"
-const Driver_Size="40"
-const Number_of_Drivers="1"
-const Water_Resistant="No"
-const Weight="385.00"
-const Bluetooth="Yes"
-const Bluetooth_Version="5"
-const Bluetooth_Codec_Support="SBC,AAC"
-const Battery="features"
-const Charging_Port="Proprietary"
-const Battery_Life="20"
+import CanvasJSReact from '../../../assets/canvasjs.react';
 
 
 
+
+
+
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+
+
+
+
+var jsondataPoints = [];
+
+var json = [
+  { "day": 1483228800000, "value": 18561.3 },
+  { "day": 1485907200000, "value": 8879.6 },
+  { "day": 1488326400000, "value": 9173.75 },
+  { "day": 1491004800000, "value": 9304.05 },
+  { "day": 1493596800000, "value": 9621.25 },
+  { "day": 1496275200000, "value": 9520.9 },
+  { "day": 1498867200000, "value": 10077.1 },
+  { "day": 1501545600000, "value": 9917.9 },
+  { "day": 1504224000000, "value": 9788.6 },
+  { "day": 1506816000000, "value": 10335.3 },
+  { "day": 1509494400000, "value": 10226.55 },
+  { "day": 1512086400000, "value": 10530.7 }
+]
+
+var sample = [
+ 
+]
+
+function Funtion2({ text }) {
+
+  fetch('https://canvasjs.com/data/gallery/react/nifty-stock-price.json')
+    .then(function (response) {
+
+      // return response.json();
+      return json;
+
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        sample.push({
+          x: new Date(data[i].day),
+          y: data[i].value
+        })
+      }
+    
+    });
+
+  for (var i = 0; i < json.length; i++) {
+    sample.push({
+      x: new Date(json[i].day),
+      y: json[i].value
+    })
+  }
+
+  return <div><p>hello</p></div>
+}
 
 function Specifications(){
+
+  const options = {
+
+    animationEnabled: true,
+    exportEnabled: true,
+    theme: "light2", //"light1", "dark1", "dark2"
+    title: {
+      text: "Simple Column Chart with Index Labels"
+    },
+    axisY: {
+      includeZero: true
+    },
+    data: [{
+      type: "column", //change type to bar, line, area, pie, etc
+      //indexLabel: "{y}", //Shows y value on all Data Points
+      indexLabelFontColor: "#5A5757",
+      indexLabelPlacement: "outside",
+      dataPoints: sample
+    }]
+  }
+
+
   return(
     <div >
-          <img src={logo} className="App-logo" alt="logo" height="150" ></img> 
-          <table>
-            <h2>General</h2>
-
-            <tr>
-              <th>Brand</th>
-              <td>{Brand}</td>
-            </tr>
-            <tr>
-              <th>Model</th>
-              <td>{Model}</td>
-            </tr>
-            <tr>
-              <th>Price</th>
-              <td>{Price}</td>
-            </tr>
-            <tr>
-              <th>Launch Date</th>
-              <td>{Launch_Date}</td>
-            </tr>
-            <tr>
-              <th>Model Number</th>
-              <td>{Model_Number}</td>
-            </tr>
-            <tr>
-              <th>Type</th>
-              <td>{Type}</td>
-            </tr>
-            <tr>
-              <th>Headphone Type</th>
-              <td>{Headphone_Type}</td>
-            </tr>
-            <tr>
-              <th>Connectivity</th>
-              <td>{Connectivity}</td>
-            </tr>
-
-
-
-            <h2>Product details</h2>
-
-            <tr>
-              <th>Microphone</th>
-              <td>{Microphone}</td>
-            </tr>
-            <tr>
-              <th>Driver Type</th>
-              <td>{Driver_Type}</td>
-            </tr>
-            <tr>
-              <th>Driver Size (mm)</th>
-              <td>{Driver_Size}</td>
-            </tr>
-            <tr>
-              <th>Number of Drivers</th>
-              <td>{Number_of_Drivers}</td>
-            </tr>
-            <tr>
-              <th>Water Resistant</th>
-              <td>{Water_Resistant}</td>
-            </tr>
-            <tr>
-              <th>Weight (g)</th>
-              <td>{Weight}</td>
-            </tr>
-
-         
-
-
-
-
-            <h2>Connectivity features</h2>
-
-            <tr>
-                <th>Bluetooth</th>
-                <td>{Bluetooth}</td>
-            </tr>
-
-            <tr>
-                <th>Bluetooth Version</th>
-                <td>{Bluetooth_Version}</td>
-            </tr>
-
-            <tr>
-                <th>Bluetooth Codec Support</th>
-                <td>{Bluetooth_Codec_Support}</td>
-            </tr>
-
-            <tr>
-                <th>Battery</th>
-                <td>{Battery}</td>
-            </tr>
-
-            <tr>
-                <th>Charging Port</th>
-                <td>{Charging_Port}</td>
-            </tr>
-
-
-            <tr>
-                <th>Battery Life (Hrs)</th>
-                <td>{Battery_Life}</td>
-            </tr>
-          </table>
+      <div style={{width: "80%",paddingLeft:"10%"}}><CanvasJSChart options={options} /></div>
+        
+          <div ><Funtion2 text={"some text"} /></div>
     </div> 
   );
 }
