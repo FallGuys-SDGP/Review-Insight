a58a2c790fa334c9ec252ab7d59d49799853a3fb
udariwelipitiya <priyanvada.welipitiya@gmail.com>
2021-04-21 12:51:17 +0530

    RI-21 #in-progress #comment created footer component

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Home from "./components/home/Home";
+import Result from "./components/results/Result";
+import "./footer.css";
+import "./components/results/Tabs.css";
+import "./components/home/Home.css";
+
+
+
+function footer(){
+  return(
+      <div>
+          footer
+      </div>
+  );
+}
+
+export default footer;
