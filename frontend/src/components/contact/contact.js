ebfcbc2efb06e460f34788fd7979e4b61145a023
Viraj Shakya Samaranayake <virajofficialinfo@gmail.com>
2021-04-23 03:28:04 +0530

    modify contact page

--- frontend/src/components/contact/contact.js
+++ frontend/src/components/contact/contact.js
@@ -2,104 +2,18 @@ import React from "react";
 import "../../components/contact/contact.css";
 import contact from "../../images/background/contact.jpg";
 
-function Contact(){
-    return(
+function Contact() {
+    return (
         <div class="contact">
-            
-            <div class ="backgroundImage">
-                 <p class="pageName">Want to get in touch? we would love to hear from you here's how you can reach us..</p> 
-                <img src ={contact} alt="contact" width="100%" height="500"/>
-
+            <h1 className="ContactDetails">Contact us</h1>
+            <div class="formset">
+            <form name="feed-form" onsubmit="return validateForm()" method="GET">
+                <input class="input" type="text" name="Name" placeholder="Name"></input>
+                <input class="input" type="text" id="email" name="youremail" placeholder="Your Email"></input>
+                <input class="input" id="message" name="yourmessage" placeholder="Write Somthing..." height="200px"></input>
+                <input class="button" type="submit" value="Submit" />
+            </form>
             </div>
-            <div calss ="form">
-                <div class ="formContent">
-                    <div class ="formContainer">
-                    <form name="feed-form" onsubmit="return validateForm()" method="GET">
-                        <div class ="firstName">         
-                            <label for="firstname">First Name</label>
-                            <input type="text" id="firstName" name="firstname" placeholder="First Name"></input>
-                        </div>
-
-                        <div class ="lastName">         
-                            <label for="lastname">Last Name</label>
-                            <input type="text" id="lastName" name="lastname" placeholder="Last Name"></input>
-                        </div>
-
-                        <div class ="email">         
-                            <label for="email">Email</label>
-                            <input type="text" id="email" name="youremail" placeholder="Your Email"></input>
-                        </div>
-
-                        <div class ="phoneNumber">         
-                            <label for="">Phone Number</label>
-                            <input type="text" id="firstname" name="firstname" placeholder="Phone Number"></input>
-                        </div>
-
-                        <label for="country">Country</label>
-                        <select id="country" name="country">
-                            <option value="australia">Australia</option>
-                            <option value="canada">Canada</option>
-                            <option value="canada">China</option>
-                            <option value="canada">India</option>
-                            <option value="canada">Japan</option>
-                            <option value="canada">Korea</option>
-                            <option value="canada">Sri Lanka</option>
-                            <option value="canada">United Kingdom</option>
-                            <option value="usa">USA</option>
-
-                        </select>
-                        
-
-                        {/* <select id="subject" name="subject">
-                            <option value="australia">Australia</option>
-                            <option value="canada">Canada</option>
-                            <option value="canada">China</option>
-                            <option value="canada">India</option>
-                            <option value="canada">Japan</option>
-                            <option value="canada">Korea</option>
-                            <option value="canada">Sri Lanka</option>
-                            <option value="canada">United Kingdom</option>
-                            <option value="usa">USA</option>
-
-                        </select> */}
-
-                        <div class ="message">         
-                            <label for="message">Message</label>
-                            <textarea id="message" name="yourmessage" placeholder="Write Somthing..." height="200px"></textarea>
-                        </div>
-
-                        <input type="submit" value="Submit"/>
-                        </form>
-                    </div>
-                </div>
-                {/* <div class ="rateus">
-                    <label for="rating">Rate Us</label>
-                    <div class="rating">
-                        <div>
-                            <input type="radio" id="excellent" name="rating" value="excellent"/>
-                            <label for="excellent">Excellent</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="good" name="rating" value="good"/>
-                            <label for="good">Good</label>
-                        </div>
-                        <div class = >
-                            <input type="radio" id="bad" name="rating" value="bad"/>
-                            <label for="bad">Bad</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="worst" name="rating" value="worst"/>
-                            <label for="worst">Worst</label>
-                        </div>
-                        
-                    </div>
-                </div> */}
-                
-            </div>
-
-                 
-
-            
         </div>
     );
 }
